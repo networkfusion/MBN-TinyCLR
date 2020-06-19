dad6a6458341d75accdc4e18e490328fde115120
Christophe Gerbier <christophe@mikrobusnet.org>
2020-06-19 22:43:47 +0200

    Added RTC3 Click

---
+++ Examples/RTC3Click/Program.cs
@@ -0,0 +1,40 @@
+﻿using MBN;
+using MBN.Modules;
+
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Examples
+{
+    class Program
+    {
+        private static RTC3Click _rtc;
+
+        private static void Main()
+        {
+            _rtc = new RTC3Click(Hardware.SC20100_1);
+
+            _rtc.EnableTrickleCharger(true);
+            _rtc.RTCEnabled = true;
+
+            DateTime currentTime = _rtc.GetDateTime();
+
+            if (currentTime.Ticks >= new DateTime(2020, 05, 26, 22, 15, 30).Ticks)
+            {
+                Debug.WriteLine("DateTime already set");
+            }
+            else
+            {
+                Debug.WriteLine("RTC not set, setting new DateTime");
+                _rtc.SetDateTime(new DateTime(2020, 05, 26, 22, 15, 30));
+            }
+
+            while (true)
+            {
+                Debug.WriteLine($"Current RTC date/time is { _rtc.GetDateTime():G}");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
