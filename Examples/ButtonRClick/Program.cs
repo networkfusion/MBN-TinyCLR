31ce6eed83357937d3565a4d25cee910a98de851
Christophe Gerbier <christophe@mikrobusnet.org>
2020-06-19 23:22:39 +0200

    Added ButtonR Click

---
+++ Examples/ButtonRClick/Program.cs
@@ -0,0 +1,32 @@
+﻿using MBN;
+using MBN.Modules;
+
+using System.Diagnostics;
+using System.Threading;
+
+namespace Examples
+{
+    public class Program
+    {
+        private static ButtonRClick _button;
+
+        public static void Main()
+        {
+            _button = new ButtonRClick(Hardware.SC20100_1) { LEDMode = ButtonRClick.Mode.ToggleWhenPressed };
+            _button.ButtonPressed += Button_ButtonPressed;
+            _button.ButtonReleased += Button_ButtonReleased;
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        static void Button_ButtonReleased(ButtonRClick sender, ButtonRClick.ButtonState state)
+        {
+            Debug.WriteLine($"Button Released Event with a state of {(state == ButtonRClick.ButtonState.Pressed ? " pressed" : " released")}");
+        }
+
+        static void Button_ButtonPressed(ButtonRClick sender, ButtonRClick.ButtonState state)
+        {
+            Debug.WriteLine($"Button Pressed Event with a state of {(state == ButtonRClick.ButtonState.Pressed ? " pressed" : " released")}");
+        }
+    }
+}
