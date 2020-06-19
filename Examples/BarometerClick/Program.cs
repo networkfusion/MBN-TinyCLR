6c9e1453b54b3bd4d082be77e6a9a2ac705a08e6
Christophe Gerbier <christophe@mikrobusnet.org>
2020-06-19 22:46:03 +0200

    Added Barometer Click

---
+++ Examples/BarometerClick/Program.cs
@@ -0,0 +1,32 @@
+﻿using MBN;
+using MBN.Modules;
+
+using System.Diagnostics;
+using System.Threading;
+
+namespace Examples
+{
+    internal class Program
+    {
+
+        private static BarometerClick _sensor;
+
+        private static void Main()
+        {
+            _sensor = new BarometerClick(Hardware.SC20100_1, BarometerClick.I2CAddress.Address1);
+            _sensor.SetRecommendedMode(BarometerClick.RecommendedModes.WeatherMonitoring);
+
+            Debug.WriteLine("-----Barometer Click Demo-----");
+
+            while (true)
+            {
+                Debug.WriteLine($"Temperature-----: {_sensor.ReadTemperature():F1} *C");
+                Debug.WriteLine($"Pressure--------: {_sensor.ReadPressure(PressureCompensationModes.SeaLevelCompensated):F1} mBar");
+                Debug.WriteLine($"Altitude--------: {_sensor.ReadAltitude():F0} meters\n");
+
+                Thread.Sleep(1000);
+            }
+
+        }
+    }
+}
