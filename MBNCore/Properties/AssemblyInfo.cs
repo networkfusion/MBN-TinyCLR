06f1ef5239eb9934bfb2622a928954866d1ac1bf
Robin Jones <robin@networkfusion.co.uk>
2021-05-10 23:30:29 +0100

    Remove core from drivers project and reference new one.

--- MBNCore/Properties/AssemblyInfo.cs
+++ MBNCore/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Click.Drivers.Core")]
+[assembly: AssemblyTitle("MBN.Click.Drivers.Core")]
 [assembly: AssemblyDescription("MikroElectronika Click Drivers Core")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DotNet.Click.Drivers.Core")]
+[assembly: AssemblyProduct("MBN.Click.Driver.Core")]
 [assembly: AssemblyCopyright("Copyright ©  NetworkFusion & MBN 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
